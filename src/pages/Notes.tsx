48fb6560e966e1ef293cd5014baf57f2ee4eb5df
Hornyai Alex <ahornyai@gmail.com>
2021-12-08 21:16:11 +0100

    centering notes

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -5,7 +5,7 @@ const Notes = () => {
       <div className="container mx-auto py-16 text-center lg:w-8/12">
         <h1 className="text-white text-3xl font-bold">Your notes</h1>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-16 justify-items-center">
           <div><NoteCard title="Grammar - Morphology" definitions={ 69 } author="sustown" /></div>
           <div><NoteCard title="Grammar - Morphology" definitions={ 69 } author="sustown" /></div>
           <div><NoteCard title="Grammar - Morphology" definitions={ 69 } author="sustown" /></div>
