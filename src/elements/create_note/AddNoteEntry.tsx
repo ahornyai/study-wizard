7ab80830ac387b53c8f9ad9cea12ae81a7a86308
Hornyai Alex <ahornyai@gmail.com>
2021-12-14 17:35:56 +0100

    file reading, minor design changes

--- src/elements/create_note/AddNoteEntry.tsx
+++ src/elements/create_note/AddNoteEntry.tsx
@@ -7,7 +7,7 @@ interface AddNoteCardProps {
 
 const AddNoteCard = (props:AddNoteCardProps) => {
     return (
-        <div className="bg-gray-800 p-3 pl-5 pr-5 grid grid-cols-2 gap-8 rounded-lg lg:mx-auto lg:w-1/4">
+        <div className="bg-gray-800 p-3 pl-5 pr-5 grid grid-cols-2 gap-8 rounded-lg lg:mx-auto">
             <Button onClick={ () => props.addNoteEntry(EntryType.NOTE) } text="Add note" size="sm" />
             <Button onClick={ () => props.addNoteEntry(EntryType.DEFINITION) } text="Add definition" size="sm" />
         </div>
--- src/elements/create_note/CreateNoteHeader.tsx
+++ src/elements/create_note/CreateNoteHeader.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from "react"
+
 import Button from "../components/Button"
 
 interface CreateNoteHeaderProps {
@@ -5,10 +7,31 @@ interface CreateNoteHeaderProps {
 }
 
 const CreateNoteHeader = ({ className = "" } : CreateNoteHeaderProps) => {
+    const fileInput = useRef<HTMLInputElement>(null);
+    const [selectedFile, setSelectedFile] = useState<File|null>(null);
+    const fileReader = new FileReader();
+        
+    fileReader.onloadend = () => {
+        console.log(fileReader.result);
+    }
+
+    useEffect(() => {
+        if (!selectedFile)
+            return
+
+        fileReader.readAsText(selectedFile);
+    })
+
     return (
-        <div className={ `bg-gray-800 p-3 rounded-lg lg:mx-auto lg:grid-cols-2 grid gap-3 lg:w-1/2 ${className}` }>
-            <input type="text" placeholder="Title" className="text-input" />
-            <Button text="Upload file" size="sm" className="justify-self-end" />
+        <div className={ `bg-gray-800 p-3 rounded-lg lg:mx-auto flex flex-wrap space-x-4 lg:w-1/2 ${className}` }>
+            <input type="text" placeholder="Title" className="text-input flex-1" />
+            <Button text="Upload file" size="sm" onClick={ () => fileInput.current?.click() } />
+
+            <input type="file" ref={ fileInput } onChange={ (e) => {
+                const fileList = e.target.files
+                
+                setSelectedFile(fileList ? fileList[0] : null)
+            } } hidden />
         </div>
     )
 }
