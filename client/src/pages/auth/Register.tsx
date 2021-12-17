559d1ad49ed8b98e387440f4d45e464f16c0e2d2
Hornyai Alex <ahornyai@gmail.com>
2021-12-17 16:35:37 +0100

    margin to toastr

--- client/src/pages/auth/Register.tsx
+++ client/src/pages/auth/Register.tsx
@@ -40,41 +40,40 @@ const Register = () => {
     }
 
     return (
-      <>
-        <ToastContainer />
-        <div className="pt-40 text-white container space-y-4 text-center">
-          <form ref={ form } onSubmit={ (e) => e.preventDefault() } className="bg-gray-800 mx-auto lg:max-w-xs md:max-w-sm rounded-lg p-5 px-10 space-y-2 mb-4">
-            <h1 className={ "text-2xl font-bold mb-5" }>Register</h1>
-            <input type="text" 
-              placeholder="Username" 
-              className="text-input w-full"
-              required
-              ref={ username } />
+      <div className="pt-40 text-white container space-y-4 text-center">
+        <ToastContainer theme="dark" style={ { marginTop: 80 } } />
 
-            <input type="email" 
-              placeholder="Email" 
-              className="text-input w-full" 
-              required
-              ref={ email } />
-              
-            <input type="password" 
-              placeholder="Password" 
-              className="text-input w-full"
-              required
-              ref={ password } />
+        <form ref={ form } onSubmit={ (e) => e.preventDefault() } className="bg-gray-800 mx-auto lg:max-w-xs md:max-w-sm rounded-lg p-5 px-10 space-y-2 mb-4">
+          <h1 className={ "text-2xl font-bold mb-5" }>Register</h1>
+          <input type="text" 
+            placeholder="Username" 
+            className="text-input w-full"
+            required
+            ref={ username } />
+
+          <input type="email" 
+            placeholder="Email" 
+            className="text-input w-full" 
+            required
+            ref={ email } />
             
-            <input type="password" 
-              placeholder="Password again" 
-              className="text-input w-full"
-              required
-              ref={ passwordAgain } />
+          <input type="password" 
+            placeholder="Password" 
+            className="text-input w-full"
+            required
+            ref={ password } />
+          
+          <input type="password" 
+            placeholder="Password again" 
+            className="text-input w-full"
+            required
+            ref={ passwordAgain } />
 
-            <Button submit={ true } text="Register" size="sm" onClick={ handleRegister } className="!mt-5" />
-          </form>
+          <Button submit={ true } text="Register" size="sm" onClick={ handleRegister } className="!mt-5" />
+        </form>
 
-          <span className="cursor-pointer select-none text-gray-100 hover:text-blue-400" onClick={ () => navigate("/forgot") }>Forgot your password?</span>
-        </div>
-      </>
+        <span className="cursor-pointer select-none text-gray-100 hover:text-blue-400" onClick={ () => navigate("/forgot") }>Forgot your password?</span>
+      </div>
     )
 }
 
