beff69edc7d3b572478d2d75413950eb55aeccbc
Hornyai Alex <ahornyai@gmail.com>
2022-01-15 20:06:53 +0100

    captcha only for auth sites

--- client/src/elements/App.tsx
+++ client/src/elements/App.tsx
@@ -17,7 +17,6 @@ import UserContextRoute from './UserContextRoute'
 import ViewNote from '../pages/dashboard/ViewNote'
 import EditNote from '../pages/dashboard/EditNote'
 import AcceptInvite from '../pages/dashboard/AcceptInvite'
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 
 const App = () => {
     const [ resource ] = useAsyncResource(syncUser, [])
@@ -27,28 +26,26 @@ const App = () => {
     return (
         <UserContext.Provider value={ { user, setUser } } >
             <Navbar />
-            
-            <GoogleReCaptchaProvider reCaptchaKey={ process.env.REACT_APP_RECAPTCHA_SITE_KEY || "" } >
-                <div className="app-holder">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        
-                        <Route path="/shared_notes" element={<UserContextRoute><Notes shared={ true } /></UserContextRoute>} />
-                        <Route path="/notes" element={<UserContextRoute><Notes shared={ false } /></UserContextRoute>} />
-                        <Route path="/notes/:id" element={<UserContextRoute><ViewNote /></UserContextRoute>} />
-                        <Route path="/notes/edit/:id" element={<UserContextRoute><EditNote /></UserContextRoute>} />
-                        <Route path="/notes/create" element={<UserContextRoute><CreateNote /></UserContextRoute>} />
-                        <Route path="/notes/invite/:id" element={<UserContextRoute><AcceptInvite /></UserContextRoute>} />
-                        
-                        
-                        <Route path="/login" element={<UserContextRoute redirectWhenAuthenticated={ true }><Login /></UserContextRoute>} />
-                        <Route path="/register" element={<UserContextRoute redirectWhenAuthenticated={ true }><Register /></UserContextRoute>} />
-                        <Route path="/forgot" element={<UserContextRoute redirectWhenAuthenticated={ true }><ForgotPassword /></UserContextRoute>} />
 
-                        <Route path="*" element={<UserContextRoute redirectWhenAuthenticated={ true }><ForgotPassword /></UserContextRoute>} />
-                    </Routes>
-                </div>
-            </GoogleReCaptchaProvider>
+            <div className="app-holder">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    
+                    <Route path="/shared_notes" element={<UserContextRoute><Notes shared={ true } /></UserContextRoute>} />
+                    <Route path="/notes" element={<UserContextRoute><Notes shared={ false } /></UserContextRoute>} />
+                    <Route path="/notes/:id" element={<UserContextRoute><ViewNote /></UserContextRoute>} />
+                    <Route path="/notes/edit/:id" element={<UserContextRoute><EditNote /></UserContextRoute>} />
+                    <Route path="/notes/create" element={<UserContextRoute><CreateNote /></UserContextRoute>} />
+                    <Route path="/notes/invite/:id" element={<UserContextRoute><AcceptInvite /></UserContextRoute>} />
+                    
+                    
+                    <Route path="/login" element={<UserContextRoute redirectWhenAuthenticated={ true } captcha={ true }><Login /></UserContextRoute>} />
+                    <Route path="/register" element={<UserContextRoute redirectWhenAuthenticated={ true } captcha={ true }><Register /></UserContextRoute>} />
+                    <Route path="/forgot" element={<UserContextRoute redirectWhenAuthenticated={ true } captcha={ true }><ForgotPassword /></UserContextRoute>} />
+
+                    <Route path="*" element={<UserContextRoute redirectWhenAuthenticated={ true }><ForgotPassword /></UserContextRoute>} />
+                </Routes>
+            </div>
         </UserContext.Provider>
     )
 }
--- client/src/elements/UserContextRoute.tsx
+++ client/src/elements/UserContextRoute.tsx
@@ -1,13 +1,15 @@
 import { useContext } from "react"
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3"
 import { Navigate } from "react-router-dom"
 import { UserContext } from "../contexts/UserContext"
 
 interface UserContextRouteProps {
     children: React.ReactComponentElement<any>
     redirectWhenAuthenticated?: boolean
+    captcha?: boolean
 }
 
-const UserContextRoute = ({ children, redirectWhenAuthenticated = false }: UserContextRouteProps) => {
+const UserContextRoute = ({ children, redirectWhenAuthenticated = false, captcha = false }: UserContextRouteProps) => {
     const { user } = useContext(UserContext)
 
     if (user.loggedIn && redirectWhenAuthenticated) {
@@ -18,7 +20,7 @@ const UserContextRoute = ({ children, redirectWhenAuthenticated = false }: UserC
         return <Navigate to="/login" />
     }
 
-    return children
+    return captcha ? <GoogleReCaptchaProvider reCaptchaKey={ process.env.REACT_APP_RECAPTCHA_SITE_KEY || "" } >{ children }</GoogleReCaptchaProvider> : children
 }
 
 export default UserContextRoute
